eb7d82441cc6b44c57c498f92fa3286f8d73d488
Selayunitasari <Sellayun25@gmail.com>
2018-03-16 10:18:17 +0700

    latihan satu halaman perdana

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//ini adalah komentar saja
+
+	echo "Hello World";
+	echo "Hello World";
+	
+	echo "sela";
+	echo "H1101161020";
+	define("STATUS", "Sendiri dulu");
+
+	$a=99
+	echo "$a";;
+?>
